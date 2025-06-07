f92599217420f62aced2c83662a4a59e3fca6bc0
Vergil <skiril502@gmail.com>
2025-06-07 16:49:40 +0300

    add EF

---
+++ Models/ScenesModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NovelProject.Models
+{
+    public class ScenesModel
+    {
+        public int id { get; set; }
+        public int id_next_scene { get; set; }
+        public string text_scene { get; set; }
+        public bool answer { get; set; }
+        public string background_scene_img { get; set; }
+        public int id_part { get; set; }
+    }
+}
--- obj/Debug/net8.0/NovelProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/NovelProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovelProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1569ce50ea3ea3c442a21285f9e05d18417b042")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovelProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovelProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
