2780ab5fc44130fedd0a0e9d2f6e84f18f6e5ba3
Vergil <skiril502@gmail.com>
2025-06-08 13:02:20 +0300

    add new test

---
+++ AlterModels/SceneViewModel.cs
@@ -0,0 +1,11 @@
+﻿using NovelProject.Models;
+
+namespace NovelProject.AlterModels
+{
+    public class SceneViewModel
+    {
+        public List<ScenesModel> Scene { get; set; }
+        public List<AnswersModel> Answers { get; set; }
+        public List<PartsModel> Parts { get; set; }
+    }
+}
