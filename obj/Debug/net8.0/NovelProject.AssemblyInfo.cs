d63179f9d926f53cc3d83f92f78ef23a66f15ff3
Disaj <103815212+Artemkca@users.noreply.github.com>
2025-06-11 00:22:56 +0300

    Зроби головне меню, налаштування

--- obj/Debug/net8.0/NovelProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/NovelProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NovelProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a2c53eb6eab848e35f98f38e1a7c36d876ade57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42de7b10cf557655b89e1a9a2983cd272b2c22c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("NovelProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NovelProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
