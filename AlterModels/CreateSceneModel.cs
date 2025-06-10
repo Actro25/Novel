b6d72bf5a0a17c83224f3514ee039bd14dbcbb69
Vergil <skiril502@gmail.com>
2025-06-10 21:40:16 +0300

    add CUD operation for Scene and Answers

---
+++ AlterModels/CreateSceneModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using NovelProject.AlterModels;
+using NovelProject.Models;
+
+public class CreateSceneModel
+{
+    public ScenesModel Scenes { get; set; } = new ScenesModel();
+    public List<AnswersModel> Answers { get; set; } = new List<AnswersModel>();
+    public int partId { get; set; }
+    public int preview_scene { get; set; }
+    public IFormFile? backgroundImage { get; set; }
+
+    [BindNever]
+    public SceneViewAllModel? AllItems { get; set; }
+}
